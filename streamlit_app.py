85e9d09ff15d21ad25bfbf868b0350c19fde58b4
dtp27 <95358349+dtp27@users.noreply.github.com>
2023-02-06 14:30:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,11 +26,14 @@ streamlit.dataframe(fruits_to_show)
 # display fruitvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# accept user input for the fruit they want to know more about
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
 # normalize/flatten the data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # organize into tabular format
 streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+

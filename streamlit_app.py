bd4de30185da31989fd2cfca47e8787a1c10288a
dtp27 <95358349+dtp27@users.noreply.github.com>
2023-02-06 14:24:16 -0500

    Update streamlit_app.py
    syntax correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,4 @@ streamlit.dataframe(fruits_to_show)
 
 # display fruitvice api response
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruitvice_response)
+streamlit.text(fruityvice_response)
